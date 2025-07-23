214ed78a3ae7952a471db480f38a6efd90763950
Ammar <ammaruchia29@gmail.com>
2025-07-23 10:33:17 +0300

    finished ModernTechSection for the main webstie

--- src/components/website/Homepage.js
+++ src/components/website/Homepage.js
@@ -1,13 +1,15 @@
 "use client";
 
 import Landing from "./Landing";
+import ModernTechSection from "./ModernTechSection";
 import Navbar from "./Navbar";
 
 function Homepage() {
   return (
-    <div className="flex flex-col bg-[#F2F2F2]">
+    <div className="flex flex-col bg-[#F2F2F2] gap-10">
       <Navbar />
       <Landing />
+      <ModernTechSection />
     </div>
   );
 }
