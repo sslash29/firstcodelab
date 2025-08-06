5ade2f5edb3cb7db9acca0cc5b190ee5378a3c27
Ammar <ammaruchia29@gmail.com>
2025-08-06 11:46:39 +0300

    changed some metadata related to the website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 export const metadata = {
-  title: "brent faiyaz",
-  description: "E-commerce app by abccommerce",
+  title: "First Code Lab",
+  description: "A Coding Institution which teaches kids from the ages of 8-50+",
 };
 
 export default function RootLayout({ children }) {
