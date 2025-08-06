f5f103196e7467ac993af865937ff602189a432a
Ammar <ammaruchia29@gmail.com>
2025-08-06 11:26:16 +0300

    changed the img to the Image tag provided by vercel

--- src/components/website/StudentProjectsSection.js
+++ src/components/website/StudentProjectsSection.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function ProjectGrid() {
   return (
     <div
@@ -5,7 +7,7 @@ export default function ProjectGrid() {
     "
     >
       <div className="w-fit">
-        <img
+        <Image
           src="/SwipeProject.png"
           alt="img"
           className="rounded-lg border w-[680px] h-[470px]"
@@ -13,12 +15,16 @@ export default function ProjectGrid() {
       </div>
       <div className="flex gap-5 flex-col gap-y-10 w-[610px] ">
         <div className="w-[300px] flex gap-5 h-fit ">
-          <img src="/pepsi.png" alt="img" className="rounded-lg border" />
-          <img src="/pizzaria.png" alt="img" className="rounded-lg border" />
+          <Image src="/pepsi.png" alt="img" className="rounded-lg border" />
+          <Image src="/pizzaria.png" alt="img" className="rounded-lg border" />
         </div>
         <div className="w-[300px] flex gap-5 h-fit ">
-          <img src="/portofolio.png" alt="img" className="rounded-lg border" />
-          <img src="/album.png" alt="img" className="rounded-lg border" />
+          <Image
+            src="/portofolio.png"
+            alt="img"
+            className="rounded-lg border"
+          />
+          <Image src="/album.png" alt="img" className="rounded-lg border" />
         </div>
       </div>
     </div>
